e5072f21221348189a346284ba1925fb40aa9826
John Oblenda <joandong2@gmail.com>
2022-03-24 21:38:38 -0400

    added tailwind css

--- src/App.js
+++ src/App.js
@@ -13,12 +13,25 @@ import { useForm } from "react-hook-form";
 function App() {
   const dispatch = useDispatch();
   const { posts, post, loading } = useSelector((state) => state.posts); // destructure/get posts, post, loading inside state post
+
+  const formNeeds = {
+    title: { required: "Title is required" },
+    body: { required: "Body is required" },
+    password: {
+      required: "Password is required",
+      minLength: {
+        value: 8,
+        message: "Password must have at least 8 characters",
+      },
+    },
+  };
+
   const {
     register,
     handleSubmit,
     reset,
     // watch,
-    // formState: { errors },
+    formState: { errors },
   } = useForm();
   const onSubmit = (data) => {
     data.hidden_id ? dispatch(updatePost(data)) : dispatch(addPost(data));
@@ -49,27 +62,81 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          {/* register your input into the hook by invoking the "register" function */}
-          <input type="hidden" {...register("hidden_id")} />
-          <input placeholder="Title" {...register("title")} />
-          <textarea placeholder="Body" {...register("body")} />
-
-          <input type="submit" value={post ? "Update" : "Submit"} />
-        </form>
-      </div>
-
-      {!loading
-        ? posts?.map((post, i) => (
-            <div key="{i}">
-              <h2>{post.title}</h2>
-              <p>{post.body}</p>
-              <button onClick={() => onEdit(post._id)}>Edit</button>
-              <button onClick={() => onDelete(post._id)}>Delete</button>
+      <div className="container mx-auto md flex items-center flex-col mt-20">
+        <div className="mb-10 mx-auto md">
+          <form onSubmit={handleSubmit(onSubmit)} className="flex ">
+            {/* register your input into the hook by invoking the "register" function */}
+            <div>
+              <input type="hidden" {...register("hidden_id")} />
             </div>
-          ))
-        : ""}
+            <div>
+              <input
+                placeholder="Title"
+                {...register("title", formNeeds.title)}
+                className="w-full border border-gray-600 rounded-md focus:outline focus:border-red-600 px-2 py-2"
+              />
+              {errors?.title && <p>{errors.title.message}</p>}
+            </div>
+            <div>
+              {" "}
+              <textarea
+                placeholder="Body"
+                {...register("body", formNeeds.body)}
+                className="w-full border border-gray-600 rounded-md focus:outline-none focus:border-red-600 px-2 py-2"
+              />
+              {errors?.body && <p>{errors.body.message}</p>}
+            </div>
+            <div>
+              <input
+                className="bg-red-600 px-6 py-2 text-white  rounded-md"
+                type="submit"
+                value={Object.keys(post).length === 0 ? "Submit" : "Update"}
+              />
+            </div>
+          </form>
+        </div>
+        <div>
+          <table class="table-auto">
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Body</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {!loading
+                ? posts?.map((post, i) => (
+                    <tr key={post._id}>
+                      <td>
+                        <h2 className="text-3xl font-bold underline">
+                          {post.title}
+                        </h2>
+                      </td>
+                      <td>
+                        <p>{post.body}</p>
+                      </td>
+                      <td>
+                        <button
+                          className="bg-green-600 px-6 py-2 text-white  rounded-md mr-2"
+                          onClick={() => onEdit(post._id)}
+                        >
+                          Edit
+                        </button>
+                        <button
+                          className="bg-red-600 px-6 py-2 text-white  rounded-md"
+                          onClick={() => onDelete(post._id)}
+                        >
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  ))
+                : ""}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
